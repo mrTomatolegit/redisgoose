987f862d1bf3d90df6f6d3c793fb486f162b0236
mrTomatolegit <alexander.trifa@yahoo.fr>
2022-03-26 15:41:34 +0300

    update index

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const Cacheman = require('recacheman-redis');
 const { Query } = require('mongoose');
 const RedisManager = require('./src/RedisManager');
 const extendQuery = require('./src/extendQuery');
+const extendModel = require('./src/extendModel');
+const warnNoInit = require('./src/warnNoInit');
+
+extendModel();
+extendQuery();
 
 module.exports = function (options) {
     const cacheman = new Cacheman(options);
     const redisManager = new RedisManager(cacheman);
-    extendQuery(redisManager);
+    Query.redisManager = redisManager;
 };
 
 module.exports.clearCache = async function (key) {
-    if (!Query.redisManager) throw new Error('RedisGoose not initialized');
+    if (!Query.redisManager) return warnNoInit();
     if (!key) return Query.redisManager.clear();
     else return Query.redisManager.del(key);
 };
+
+Object.defineProperty(module.exports, 'redisManager', {
+    get: function () {
+        return Query.redisManager;
+    }
+});
